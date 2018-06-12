fffb41e2f1483522ff7fb33736fa6efcd05e7c46
Mark <markclare1992@gmail.com>
2018-06-12 16:03:16 +0100

    init

--- clean_wiki.py
+++ clean_wiki.py
@@ -2,3 +2,28 @@ from pymongo import MongoClient
 import googlemaps, numpy, wikipedia, wptools, re, html, config
 from googlemaps import convert
 from bs4 import BeautifulSoup, NavigableString
+
+#make everything lower case when comparing
+unit_strings = ['m', 'metres', 'y', 'yards', 'yds', 'yd', 'ft', 'feet']
+client = MongoClient()
+if client:
+    matches = client.whoscored.matches
+    stadiums = client.whoscored.stadiums
+    wiki = client.whoscored.wiki
+
+def strip_html(src):
+    p = BeautifulSoup(src, 'lxml')
+    text = p.findall(text=lambda text:isinstance(text, NavigableString))
+    return u" ".join(text)
+
+
+def clean_dimensions(dimension_string):
+    return
+
+
+def clean_surface(surface_string):
+    return
+
+
+def clean_wiki_data(wiki_data):
+    return
--- scrape.py
+++ scrape.py
@@ -2,19 +2,13 @@ from pymongo import MongoClient
 import googlemaps, numpy, wikipedia, wptools, re, html, config
 from googlemaps import convert
 from bs4 import BeautifulSoup, NavigableString
-#make everything lower case when comparing
-unit_strings = ['m', 'metres', 'y', 'yards', 'yds', 'yd', 'ft', 'feet']
+
 gmaps = googlemaps.Client(key = config.gmaps_api_key)
 client = MongoClient()
 if client:
     matches = client.whoscored.matches
     stadiums = client.whoscored.stadiums
     wiki = client.whoscored.wiki
-
-def strip_html(src):
-    p = BeautifulSoup(src, 'lxml')
-    text = p.findall(text=lambda text:isinstance(text, NavigableString))
-    return u" ".join(text)
 
 
 def scrape(stadium, overwrite=False):
